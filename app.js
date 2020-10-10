a97030bbc8eb8f02678df39d0725e1ec8a86c0c8
pgauin01 <praful.gaur.2@gmail.com>
2020-10-10 14:09:54 +0530

    react server

--- app.js
+++ app.js
@@ -95,4 +95,12 @@ app.all('*', (req, res, next) => {
 
 app.use(gloablErrorhandler);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*' , (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'))
+  })
+}
+
 module.exports = app;
--- server.js
+++ server.js
@@ -29,13 +29,7 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
 
-  app.get('*' , (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'client' , 'build' , 'index.html'))
-  })
-}
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
