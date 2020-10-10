9d8a20904c42f6aea1ed3b74e2d13b9f62b7c203
pgauin01 <praful.gaur.2@gmail.com>
2020-10-10 15:26:05 +0530

    react server

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const express = require('express');
-const path = require('path');
 
 dotenv.config({
   path: './config.env',
